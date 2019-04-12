8abcab91add54cffba31bca3980abb875f2c7ada
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-04-12 15:13:15 +0200

    I've add the die

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,5 +1,18 @@
+import java.util.Random;
+
 public class Die {
     private int faceValue;
 
+    public Die(){
+        roll();
+    }
 
+    public void roll(){
+        Random r = new Random();
+        faceValue = r.nextInt(6) + 1;
+    }
+
+    public int getFaceValue(){
+        return faceValue;
+    }
 }
