919d547017d12f4380ebd470886e9fae797e9c2d
yoann0000 <47739072+yoann0000@users.noreply.github.com>
2019-04-12 15:53:26 +0200

    board tests

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,2 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTest {
+
+    @Test
+    public void ConstructionTest(){
+        Board testBoard = new Board();
+        assertEquals("Go", testBoard.getSquares().get(0).getName());
+
+        for (int i = 1; i < 40; i++) {
+            assertEquals("Square " + i, testBoard.getSquares().get(i).getName());
+        }
+    }
+
+    @Test
+    public void BoardNumberTest(){
+        Board testBoard = new Board();
+
+        assertEquals("Square 39", testBoard.getSquares().get(39).getName());
+    }
+
+    @Test
+    public void BoardSquareNumTest(){
+        assertEquals(40, new Board().getSquares().size());
+    }
 }
