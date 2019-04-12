b6dbdaa1af27130cc94d1161dc9b19704a8e5cee
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-04-12 16:27:31 +0200

    player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,9 +2,9 @@ public class Player {
     private String name;
     private Piece piece;
 
-    public Player(String n, Piece p){
+    public Player(String n, Board b){
         name = n;
-        piece = p;
+        piece = new Piece("test", null);
     }
 
     public void takeTurn(Die[] d,Board b){
@@ -19,7 +19,7 @@ public class Player {
         }
 
         old = piece.getSquare();
-        nw  = piece.getSquare();
+        nw  = b.getSquares().get(value);
 
         piece.setSquare(nw);
 
