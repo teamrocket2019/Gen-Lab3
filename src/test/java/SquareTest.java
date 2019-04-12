bd8f3411977922e90e5f3380543272fba4536cce
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-04-12 15:42:34 +0200

    I had a test to Square

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SquareTest {
+
+    @Test
+    public void aSquareShouldHaveAName(){
+        Square s1 = new Square("Ceci est mon nom");
+        String s2 = "Ceci est mon nom";
+        assertEquals(s1.getName(), s2);
+    }
+
+
 }
