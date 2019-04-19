0a3113086efabb02831d0602bf268671075eae43
Yoann0000 <yoann.rohrbasser@bluewin.ch>
2019-04-19 13:49:03 +0200

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,9 +2,9 @@ public class Player {
     private String name;
     private Piece piece;
 
-    public Player(String n, Piece p){
-        name = n;
-        piece = p;
+    public Player(String playerName, String pieceName, Square start){
+        name = playerName;
+        piece = new Piece(pieceName, start);
     }
 
     public void takeTurn(Die[] d,Board b){
