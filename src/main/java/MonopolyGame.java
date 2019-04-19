731b8e42af3ef3c5efdeb1bbc57822a44e44eeb2
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-04-19 14:56:42 +0200

    I've add the implementation of MonopolyGame

--- src/main/java/MonopolyGame.java
+++ src/main/java/MonopolyGame.java
@@ -1,15 +1,32 @@
 import java.util.LinkedList;
 
 public class MonopolyGame {
-    private Die d1;
-    private Die d2;
+    private Die[] dice;
     private Board board;
     private LinkedList<Player> players;
 
-    public MonopolyGame(){
-        d1 = new Die();
-        d2 = new Die();
+    public MonopolyGame(int nbPlayer){
+        dice = new Die[]{new Die(), new Die()};
         board = new Board();
         players = new LinkedList<>();
+        for(int i = 0; i < nbPlayer; ++i){
+            players.add(
+                    new Player("Player" + i, "Piece" + i, board.getSquare(null, 0)));
+        }
+    }
+
+    public void playGame(){
+        for(int roundCount = 0; roundCount < 20; ++roundCount){
+            playRound(players.get(roundCount % players.size()));
+        }
+    }
+
+    public void playRound(Player p){
+        p.takeTurn(dice, board);
+    }
+
+    public static void main(String ... args){
+        MonopolyGame mg = new MonopolyGame(Integer.parseInt(args[0]));
+        mg.playGame();
     }
 }
